27614a32c4bb5e5602ae858582e3bdb1e93a205e
Mohamed Fawzy <mohamed.f.elmorsi@gmail.com>
2020-10-04 23:30:44 +0200

    setup server and config

--- server/server.js
+++ server/server.js
@@ -203,11 +203,13 @@ const async = require("async");
 require("dotenv").config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(express.static("client/build"));
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -612,27 +614,35 @@ app.post("/api/users/update_profile", auth, (req, res) => {
 //              SITE
 //=================================
 
-app.get('/api/site/site_data',(req,res)=>{
-    Site.find({},(err,site)=>{
-        if(err) return res.status(400).send(err);
-        res.status(200).send(site[0].siteInfo)
-    });
+app.get("/api/site/site_data", (req, res) => {
+  Site.find({}, (err, site) => {
+    if (err) return res.status(400).send(err);
+    res.status(200).send(site[0].siteInfo);
+  });
 });
 
-app.post('/api/site/site_data',auth,admin,(req,res)=>{
-    Site.findOneAndUpdate(
-        { name: 'Site'},
-        { "$set": { siteInfo: req.body }},
-        { new: true },
-        (err,doc )=>{
-            if(err) return res.json({success:false,err});
-            return res.status(200).send({
-                success: true,
-                siteInfo: doc.siteInfo
-            })
-        }
-    )
-})
+app.post("/api/site/site_data", auth, admin, (req, res) => {
+  Site.findOneAndUpdate(
+    { name: "Site" },
+    { $set: { siteInfo: req.body } },
+    { new: true },
+    (err, doc) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true,
+        siteInfo: doc.siteInfo,
+      });
+    }
+  );
+});
+
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`Server Running at ${port}`);
