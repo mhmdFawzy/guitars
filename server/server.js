2e5642d5b5875ab9dc453395462dbe885f714174
Mohamed Fawzy <mohamed.f.elmorsi@gmail.com>
2020-10-05 06:09:13 +0200

    change dirname LINK

--- server/server.js
+++ server/server.js
@@ -638,8 +638,8 @@ app.post("/api/site/site_data", auth, admin, (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
