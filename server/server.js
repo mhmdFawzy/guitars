598b30ec4f4ea101f3da24a98e7258cc147ccbdb
Mohamed Fawzy <mohamed.f.elmorsi@gmail.com>
2020-10-05 06:01:24 +0200

    first push

--- server/server.js
+++ server/server.js
@@ -209,7 +209,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.use(express.static("/client/build"));
+app.use(express.static("client/build"));
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -638,7 +638,7 @@ app.post("/api/site/site_data", auth, admin, (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
