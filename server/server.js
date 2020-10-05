48bd408661dd479ef83d7fdc8d4b8d8a8cc28c96
Mohamed Fawzy <mohamed.f.elmorsi@gmail.com>
2020-10-05 07:10:45 +0200

    fix prefix

--- server/server.js
+++ server/server.js
@@ -16,8 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-// app.use(express.static("client/build"));
-app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.static("client/build"));
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -446,13 +445,10 @@ app.post("/api/site/site_data", auth, admin, (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
-  // app.get("/*", (req, res) => {
-  //   res.sendFile(
-  //     path.resolve(__dirname, "../../client", "build", "index.html")
-  //   );
-  // });
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  app.get("/*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "../../client", "build", "index.html")
+    );
   });
 }
 
