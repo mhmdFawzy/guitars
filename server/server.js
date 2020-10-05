31c4c34564f30cd80c167150c7876b8d785df881
Mohamed Fawzy <mohamed.f.elmorsi@gmail.com>
2020-10-05 06:12:51 +0200

    change dirname static

--- server/server.js
+++ server/server.js
@@ -639,7 +639,8 @@ app.post("/api/site/site_data", auth, admin, (req, res) => {
 if (process.env.NODE_ENV === "production") {
   const path = require("path");
   app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    // res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    app.use(express.static(path.join(__dirname, "../client/build")));
   });
 }
 
